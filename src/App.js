b8a6c4446ca3f641e17c43c5b07f36bc4180aee0
Samama Aziz <122434021+Samama-Aziz@users.noreply.github.com>
2023-05-27 15:18:54 +0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Pagedata from './components/Pagedata';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Pagedata/>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <Routes>
+            <Route exact path="/">
+            <Pagedata name="Home" />
+            </Route>
+
+            <Route path="/about">
+              <Pagedata name="About" />
+            </Route>
+
+            <Route path="/services">
+              <Pagedata name="Services" />
+            </Route>
+
+            <Route path="/contact">
+              <Pagedata name="Contact" />
+            </Route>
+          </Routes>
+        </React.Fragment>
+      </BrowserRouter>
     </>
   );
 }
