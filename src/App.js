d625b59ea985819a20810168e18916365a555713
Samama Aziz <122434021+Samama-Aziz@users.noreply.github.com>
2023-05-27 21:22:34 +0500

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
+
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
 import Pagedata from './components/Pagedata';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <React.Fragment>
-          <Navbar />
-          <Routes>
-            <Route exact path="/">
-            <Pagedata name="Home" />
-            </Route>
-
-            <Route path="/about">
-              <Pagedata name="About" />
-            </Route>
-
-            <Route path="/services">
-              <Pagedata name="Services" />
-            </Route>
-
-            <Route path="/contact">
-              <Pagedata name="Contact" />
-            </Route>
-          </Routes>
-        </React.Fragment>
+        <Routes>
+          <Route exact path="/" element={<><Navbar /> <Pagedata name="Home" backgroundColor="#222" /></>} />
+          <Route path="/about" element={<><Navbar /> <Pagedata name="About" backgroundColor=" #444444"/></>} />
+          <Route path="/services" element={<><Navbar /> <Pagedata name="Services" backgroundColor="#556B2F"/></>} />
+          <Route path="/contact" element={<><Navbar /> <Pagedata name="Contact" backgroundColor="#090979" /></>} />
+        </Routes>
       </BrowserRouter>
     </>
   );
 }
 
 export default App;
+
